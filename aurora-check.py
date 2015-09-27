5fc99a99dc232464819d52ca89697a43f5e32971
Marcus Windmark <marcus.windmark@gmail.com>
2015-09-27 19:55:37 +0200

    Parse aurora website. Note all forecasts with kp over threshold

--- aurora-check.py
+++ aurora-check.py
@@ -1,7 +1,40 @@
 from urllib.request import urlopen
+from bs4 import BeautifulSoup
+from dateutil.parser import parse
+import re
 
 url = 'http://www.aurora-service.eu/aurora-forecast/'
 response = urlopen(url)
 html = response.read()
-print(html)
+#print(html)
 
+soup = BeautifulSoup(html, 'html.parser')
+unformatedTable = soup.find_all('pre')[0].get_text()
+tableList = unformatedTable.replace(" ", "").split('\n')
+
+unformatedDates = tableList.pop(0)
+dateLength = 5
+formatedDates = [unformatedDates[i:i+dateLength] for i in range(0, len(unformatedDates), dateLength)]
+for obj in enumerate(formatedDates, start=0):
+	formatedDates[obj[0]] = parse(obj[1])
+
+
+kpThreshold = 6
+alertList = list()
+
+
+forecastInfo = list()
+for data in tableList:
+	if data:
+		unformated = data.split('UT')
+		hours = unformated[0].split('-')
+		values = re.findall('.', unformated[1])
+		info = hours[0], hours[1], values
+		forecastInfo.append(info)
+
+		for kp in values:
+			if int(kp) > kpThreshold:
+				alertList.append(info)
+
+
+print(alertList)
