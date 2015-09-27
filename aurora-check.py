2475965d4e279b5869d4cfa703b69ad1b2537c88
Marcus Windmark <marcus.windmark@gmail.com>
2015-09-27 20:05:14 +0200

    Separated days into different objects

--- aurora-check.py
+++ aurora-check.py
@@ -15,26 +15,27 @@ tableList = unformatedTable.replace(" ", "").split('\n')
 unformatedDates = tableList.pop(0)
 dateLength = 5
 formatedDates = [unformatedDates[i:i+dateLength] for i in range(0, len(unformatedDates), dateLength)]
-for obj in enumerate(formatedDates, start=0):
+for obj in enumerate(formatedDates, start = 0):
 	formatedDates[obj[0]] = parse(obj[1])
 
 
-kpThreshold = 6
+kpThreshold = 3
 alertList = list()
+forecastList = list()
 
-
-forecastInfo = list()
 for data in tableList:
 	if data:
 		unformated = data.split('UT')
 		hours = unformated[0].split('-')
 		values = re.findall('.', unformated[1])
-		info = hours[0], hours[1], values
-		forecastInfo.append(info)
+		for obj in enumerate(values, start = 0):
+			kp = int(obj[1])
+			forecast = formatedDates[obj[0]], hours[0], hours[1], kp
+			forecastList.append(forecast)
 
-		for kp in values:
-			if int(kp) > kpThreshold:
-				alertList.append(info)
+			if kp >= kpThreshold:
+				alertList.append(forecast)
+		
 
 
 print(alertList)
