24620235ff6e39f20d9ea64813a0eb58571dbaed
Marcus Windmark <marcus.windmark@gmail.com>
2015-10-08 17:43:31 +0200

    Extracted config settings to separate file

--- feed.py
+++ feed.py
@@ -1,28 +1,31 @@
 #!/usr/bin/python
 
 import datetime
+import configparser
 from feedgen.feed import FeedGenerator
 from pushbullet import Pushbullet
 
 from check import Aurora
 
+config = configparser.ConfigParser()
+config.read('config.ini')
 
-url = 'http://www.aurora-service.eu/aurora-forecast/'
-kpThreshold = 6
+url = config['DEFAULT']['AuroraURL']
+kpThreshold = int(config['DEFAULT']['KpThreshold'])
+pbKey = config['DEFAULT']['PushbulletKey']
+
 alertList = Aurora(url, kpThreshold).getAlertList()
 
 today = datetime.date.today()
+utcTime = datetime.datetime.utcnow()
 
 description = ''
 for item in alertList:
 	title = item[0].strftime('%d/%m/%Y') + ': ' + "UTC" + item[1] + '-' + item[2]
 	description	+= title + " - kp" + str(item[3]) + "\n"
 
-utcTime = datetime.datetime.utcnow()
-
 if len(alertList) > 0:
-	pbKey = '7962b1649658b184a9ecfb78844f7384'
 	pb = Pushbullet(pbKey)
-	text = "Aurora status over kp" + str(kpThreshold) + " at UTC " + utcTime.strftime("%H:%M")
+	title = "Aurora status over kp" + str(kpThreshold) + " at UTC " + utcTime.strftime("%H:%M")
 	pb_channel = pb.channels[0]
-	pb_channel.push_note(text, description)
+	pb_channel.push_note(title description)
