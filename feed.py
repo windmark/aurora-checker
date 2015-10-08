2e5130edd1a2975d9b868ac7dec19bd0d58c93b9
root <root@website.(none)>
2015-10-08 17:10:29 +0200

    Changed to using pushbullet channel

--- feed.py
+++ feed.py
@@ -15,11 +15,14 @@ today = datetime.date.today()
 
 description = ''
 for item in alertList:
-	title = item[0].strftime('%d/%m/%Y') + ': ' + item[1] + '-' + item[2]
+	title = item[0].strftime('%d/%m/%Y') + ': ' + "UTC" + item[1] + '-' + item[2]
 	description	+= title + " - kp" + str(item[3]) + "\n"
 
+utcTime = datetime.datetime.utcnow()
 
 if len(alertList) > 0:
 	pbKey = '7962b1649658b184a9ecfb78844f7384'
 	pb = Pushbullet(pbKey)
-	push = pb.push_note("Aurora status over kp" + str(kpThreshold), description)
+	text = "Aurora status over kp" + str(kpThreshold) + " at UTC " + utcTime.strftime("%H:%M")
+	pb_channel = pb.channels[0]
+	pb_channel.push_note(text, description)
